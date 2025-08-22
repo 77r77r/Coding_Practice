ba6116a86c8bbb40a5e924b895795f17e5b0bbaa
77r77r <32328758+77r77r@users.noreply.github.com>
2025-08-22 14:40:18 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.03 ms, Memory: 71.1 MB -BaekjoonHub

--- 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.java
+++ 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.java
@@ -1,13 +1,9 @@
 class Solution {
     public int[] solution(String[] keyinput, int[] board) {
-        
         // up을 누른다면 캐릭터의 좌표는 [0, 1]
         // down을 누른다면 [0, -1],
         // left를 누른다면 [-1, 0], 
         //right를 누른다면 [1, 0]
-        
-        int[][] move = new int[board[0]][board[1]];
-        
         int[] dx = {0, 0, -1, 1};   // u, d, l, r
         int[] dy = {1, -1, 0, 0};
         
@@ -16,27 +12,22 @@ class Solution {
         int[] SIZE = {board[0] / 2, board[1] / 2};
         
         for (String key : keyinput) {
-            System.out.println(key);
             switch (key) {
                 case "up" : 
-                    if (ny < SIZE[1] )
-                        ny += dy[0];
+                    if (ny < SIZE[1]) ny += dy[0];
                     break;
                 case "down" :
-                    if (ny > -SIZE[1])
-                        ny += dy[1];
+                    if (ny > -SIZE[1]) ny += dy[1];
                     break;
                 case "left" : 
-                    if (nx > -SIZE[0])
-                        nx += dx[2];
+                    if (nx > -SIZE[0]) nx += dx[2];
                     break;
                 case "right" :
-                    if (nx < SIZE[0])
-                        nx += dx[3];
+                    if (nx < SIZE[0]) nx += dx[3];
                     break;
             }
         }
-        int[] answer = {nx, ny};
-        return answer;
+       
+        return new int[]{nx, ny};
     }
 }
