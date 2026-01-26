56b51b3d845883d394029c2b064d843d314cdb19
Nomi <32328758+77r77r@users.noreply.github.com>
2026-01-26 09:50:20 +0900

    [Silver V] Title: 분수찾기, Time: 148 ms, Memory: 16284 KB -BaekjoonHub

---
+++ 백준/Silver/1193. 분수찾기/분수찾기.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int X = Integer.parseInt(br.readLine());
+
+		int head = 0;
+		int body = 0;
+
+		int cnt = 0;
+		int l = 1;
+		
+		for (int x = 0; x < X; x++) {
+
+			if (cnt == l) {
+				l++;
+				cnt = 0;
+			}
+
+			if (l % 2 != 0) {
+				head = l - cnt;
+				body = 1 + cnt;
+			} else {
+				head = 1 + cnt;
+				body = l - cnt;
+			}
+
+			cnt++;
+		}
+
+		bw.write(head + "/" + body);
+		br.close();
+		bw.close();
+	}
+}
