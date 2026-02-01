37bca66989f2f2b0d92b54d3c5b0d87030d509b7
Nomi <32328758+77r77r@users.noreply.github.com>
2026-02-01 12:45:55 +0900

    [Silver III] Title: 체스판 다시 칠하기, Time: 136 ms, Memory: 18868 KB -BaekjoonHub

---
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
@@ -0,0 +1,112 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int height = Integer.parseInt(st.nextToken());
+		int width = Integer.parseInt(st.nextToken());
+
+		String[] boards = new String[height];
+
+		for (int i = 0; i < height; i++) {
+			boards[i] = br.readLine();
+		}
+
+		// 10 13
+
+		int minCount = 3000;
+		int change = 0;
+
+		for (int h = 0; h < height - 7; h++) {
+			// 높이 이동
+			for (int w = 0; w < width - 7; w++) {
+				// 가로 분할해서 이동하기
+				change = 0;
+				int top = 1;
+
+				for (int k = h; k < h + 8; k++) {
+					char start = 'B';
+					char lineStart = start;
+
+					String line = boards[k];
+					// 8개 뽑기
+					char[] temp = line.substring(w, w + 8).toCharArray();
+
+					if (top % 2 == 0) {
+						lineStart = start == 'B' ? 'W' : 'B';
+					} else {
+						lineStart = start;
+					}
+
+					// bw.write(lineStart+ "\n");
+
+					for (int i = 0; i < 8; i++) {
+						if (i % 2 == 0) {
+							if (temp[i] != lineStart) {
+								change++;
+							}
+						} else {
+							if (temp[i] == lineStart) {
+								change++;
+							}
+						}
+					}
+					top++;
+				}
+
+				if (minCount > change) {
+					minCount = change;
+				}
+
+				// W
+
+				change = 0;
+				top = 1;
+
+				for (int k = h; k < h + 8; k++) {
+					char start = 'W';
+					char lineStart = start;
+
+					String line = boards[k];
+					// 8개 뽑기
+					char[] temp = line.substring(w, w + 8).toCharArray();
+
+					if (top % 2 == 0) {
+						lineStart = start == 'B' ? 'W' : 'B';
+					} else {
+						lineStart = start;
+					}
+
+					// bw.write(lineStart+ "\n");
+
+					for (int i = 0; i < 8; i++) {
+						if (i % 2 == 0) {
+							if (temp[i] != lineStart) {
+								change++;
+							}
+						} else {
+							if (temp[i] == lineStart) {
+								change++;
+							}
+						}
+					}
+					top++;
+				}
+
+				if (minCount > change) {
+					minCount = change;
+				}
+			}
+		}
+
+		bw.write(minCount + "");
+
+		br.close();
+		bw.close();
+	}
+}
