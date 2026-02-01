ff0c86497474bf05770f8ab51cd0e9a090bb807e
Nomi <32328758+77r77r@users.noreply.github.com>
2026-02-01 21:34:39 +0900

    [Bronze II] Title: 블랙잭, Time: 96 ms, Memory: 14376 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.java
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -17,18 +17,11 @@ class Main {
 			.mapToInt(Integer::parseInt)
 			.toArray();
 
-		Arrays.sort(cards);
 		int maxSum = 0;
 
-		//i 5
 		for (int i = n - 1; i >= 2; i--) {
-			// 모든 경우의 수 해보기
-			// 9 8 7 6 5
 			for (int j = i - 1; j >= 1; j--) {
-				// i = 5, j = 4
-				// 8 7 6 5
 				for (int k = j - 1; k >= 0; k--) {
-					// i = 5, j = 4, k =3
 					int sum = cards[i] + cards[j] + cards[k];
 					if (sum <= target) {
 						maxSum = maxSum < sum ? sum : maxSum;
