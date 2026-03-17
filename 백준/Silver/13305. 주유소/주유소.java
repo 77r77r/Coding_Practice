9a23fd09a70ba0f194610782d333f624bf61f26e
Nomi <32328758+77r77r@users.noreply.github.com>
2026-03-17 22:14:29 +0900

    [Silver III] Title: 주유소, Time: 316 ms, Memory: 31296 KB, Score: 17 point -BaekjoonHub

---
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		StringTokenizer st;
+
+		int n = Integer.parseInt(br.readLine());
+		int[] road = new int[n - 1];
+		int[] price = new int[n];
+		int money = 0;    // 비용
+
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n - 1; i++) {
+			road[i] = Integer.parseInt(st.nextToken());
+		}
+
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n; i++) {
+			price[i] = Integer.parseInt(st.nextToken());
+		}
+
+		// 최소 다음 구역까지 넣을 수 밖에 없음
+		money += price[0] * road[0];
+
+		for (int i = 1; i < n - 1; i++) {
+			int min = Math.min(price[i], price[i - 1]);
+			money += min * road[i];
+		}
+
+		bw.write(String.valueOf(money));
+
+		bw.flush();
+		br.close();
+		bw.close();
+	}
+}
