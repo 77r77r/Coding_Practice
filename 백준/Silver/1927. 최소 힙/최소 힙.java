699c6676a3b162d08e7420eb5569414ed0077d3e
Nomi <32328758+77r77r@users.noreply.github.com>
2026-03-27 23:11:01 +0900

    [Silver II] Title: 최소 힙, Time: 276 ms, Memory: 26476 KB -BaekjoonHub

--- 백준/Silver/1927. 최소 힙/최소 힙.java
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -2,88 +2,30 @@ import java.io.*;
 import java.util.*;
 
 class Main {
-	static int[] heap;
-	static int size = 0;
-
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+		PriorityQueue<Integer> pq = new PriorityQueue<>();
+
 		int n = Integer.parseInt(br.readLine());
-		heap = new int[n];
 
 		while (n-- > 0) {
 			int num = Integer.parseInt(br.readLine());
 
 			if (num == 0) {
-				bw.write(poll() + "\n");
+				if (!pq.isEmpty()) {
+					bw.write(pq.poll() +"\n");
+				} else {
+					bw.write("0\n");
+				}
 			} else {
-				add(num);
+				pq.add(num);
 			}
 		}
 
 		bw.flush();
 		br.close();
 		bw.close();
-	}
-
-	static void add(int value) {
-		heap[size] = value;
-		int i = size++;
-
-		while (i > 0) {
-			int root = (i - 1) / 2;
-
-			if (heap[root] <= heap[i]) {
-				break;
-			}
-
-			int temp = heap[root];
-			heap[root] = heap[i];
-			heap[i] = temp;
-
-			i = root;
-		}
-	}
-
-	static int poll() {
-		if (size == 0) {
-			return 0;
-		}
-
-		int max = heap[0];
-
-		heap[0] = heap[size - 1];
-		size--;
-
-		int i = 0;
-
-		// 정렬
-		while (true) {
-			int left = 2 * i + 1;
-			int right = 2 * i + 2;
-			int min = i;
-
-			if (left < size && heap[left] < heap[min]) {
-				min = left;
-			}
-
-			if (right < size && heap[right] < heap[min]) {
-				min = right;
-			}
-
-			// 변화 없음
-			if (min == i) {
-				break;
-			}
-
-			int temp = heap[i];
-			heap[i] = heap[min];
-			heap[min] = temp;
-
-			i = min;
-		}
-
-		return max;
 	}
 }
