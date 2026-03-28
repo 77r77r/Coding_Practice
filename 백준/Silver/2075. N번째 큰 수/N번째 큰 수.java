c011571df28fb5188d459faf47eb1f33e3b0e1ba
Nomi <32328758+77r77r@users.noreply.github.com>
2026-03-28 21:20:41 +0900

    [Silver III] Title: N번째 큰 수, Time: 724 ms, Memory: 218296 KB -BaekjoonHub

--- 백준/Silver/2075. N번째 큰 수/N번째 큰 수.java
+++ 백준/Silver/2075. N번째 큰 수/N번째 큰 수.java
@@ -2,28 +2,30 @@ import java.io.*;
 import java.util.*;
 
 class Main {
+	static int[] heap;
+	static int size = 0;
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
 		StringTokenizer st;
 
-		PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
-
 		int n = Integer.parseInt(br.readLine());
+		heap = new int[n * n];
 
 		for (int i = 0; i < n; i++) {
 			st = new StringTokenizer(br.readLine());
 
 			for (int j = 0; j < n; j++) {
-				pq.add(Integer.parseInt(st.nextToken()));
+				add(Integer.parseInt(st.nextToken()));
 			}
 		}
 
 		int value = 0;
 
 		for (int i = 0; i < n; i++) {
-			value = pq.poll();
+			value = poll();
 		}
 
 		bw.write(String.valueOf(value));
@@ -31,5 +33,67 @@ class Main {
 		bw.flush();
 		br.close();
 		bw.close();
+	}
+
+	static void add(int n) {
+		heap[size] = n;
+		int i = size++;
+
+		while (true) {
+			int parent = (i - 1) / 2;
+			int maximum = i;
+
+			if (heap[parent] < heap[i]) {
+				maximum = parent;
+			}
+
+			if (maximum == i) {
+				break;
+			}
+
+			int temp = heap[parent];
+			heap[parent] = heap[i];
+			heap[i] = temp;
+
+			i = parent;
+		}
+	}
+
+	static int poll() {
+		if (size == 0) {
+			return 0;
+		}
+
+		int max = heap[0];
+		size--;
+		heap[0] = heap[size];
+
+		int idx = 0;
+
+		while (true) {
+			int left = (idx * 2) + 1;
+			int right = (idx * 2) + 2;
+			int maximum = idx;
+
+			if (left < size && heap[left] > heap[maximum]) {
+				maximum = left;
+			}
+
+			if (right < size && heap[right] > heap[maximum]) {
+				maximum = right;
+			}
+
+			if (maximum == idx) {
+				break;
+			}
+
+			int temp = heap[maximum];
+			heap[maximum] = heap[idx];
+			heap[idx] = temp;
+
+			idx = maximum;
+		}
+
+		return max;
 	}
 }
