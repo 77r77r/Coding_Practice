60f5cea8c86d025692cbc3827e804467d1af6058
Nomi <32328758+77r77r@users.noreply.github.com>
2026-04-05 23:40:23 +0900

    [Gold V] Title: 뱀과 사다리 게임, Time: 108 ms, Memory: 14380 KB -BaekjoonHub

---
+++ 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.java
@@ -0,0 +1,82 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+
+		Map<Integer, Integer> ladder = new HashMap<>();
+		Map<Integer, Integer> snake = new HashMap<>();
+		boolean[] visited = new boolean[101];
+
+		// 사다리
+		for (int i = 0; i < n; i++) {
+			st = new StringTokenizer(br.readLine());
+			ladder.put(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+		}
+
+		// 뱀
+		for (int i = 0; i < m; i++) {
+			st = new StringTokenizer(br.readLine());
+			snake.put(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+		}
+
+		Queue<Integer> q = new LinkedList<>();
+		q.add(1);
+		int count = 0;
+
+		while (!q.isEmpty()) {
+			int size = q.size();
+
+			while (size-- > 0) {
+
+				int now = q.poll();
+
+				for (int i = 0; i < 6; i++) {
+					int dice = now + i + 1;
+					int move = dice;
+
+					// 100번째칸 이상이면 통과
+					if (move > 100) {
+						continue;
+					}
+
+					// 이동한 칸에 뱀이 있는지 확인
+					if (snake.containsKey(dice)) {
+						move = snake.get(dice);
+					}
+
+					// 이동한 칸에 사다리가 있는지 확인
+					if (ladder.containsKey(dice)) {
+						move = ladder.get(dice);
+					}
+
+					if (visited[move]) {
+						continue;
+					}
+
+					visited[move] = true;
+					q.add(move);
+				}
+			}
+
+			count++;
+
+			if (visited[100]) {
+				break;
+			}
+		}
+
+		bw.write(count + "");
+
+		bw.flush();
+		br.close();
+		bw.close();
+	}
+}
